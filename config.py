0c1657683f5db88fe65625fe737d9fdd5eb84db8
Emma <seebynum@gmail.com>
2025-04-14 10:28:55 +0800

    config

--- config.py
+++ config.py
@@ -1,15 +1,10 @@
-import promptlayer
 import streamlit as st
+import openai
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 
+# OpenAI
+openai.api_key = st.secrets.get("OPENAI_API_KEY", "")
 
-# openai
-# I use promptlayer to log my requests
-promptlayer.api_key = st.secrets.get("PROMPTLAYER_API_KEY", "")
-openai = promptlayer.openai
-openai_api_key = st.secrets.get("OPENAI_API_KEY", "")
-openai.api_key = openai_api_key
-
-# anthropic
+# Anthropic
 anthropic_api_key = st.secrets.get("ANTHROPIC_API_KEY", "")
 anthropic = Anthropic(api_key=anthropic_api_key)
